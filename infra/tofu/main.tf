366aa4a8e11dfa0e9088570b590863ce576bf380
Ricky Ng-Adam <rngadam@gmail.com>
2026-02-02 12:54:36 -0500

    chore: remove infrastructure files (moved to modern-resume-infra)

--- infra/tofu/main.tf
+++
@@ -1,6 +0,0 @@
-# This file is intentionally left mostly empty as resources are split into specific files.
-# You can add global resources or locals here.
-
-locals {
-  project_name = "modern-resume"
-}
